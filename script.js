01294c05c3c3c580054d731c75c21adaf81ec82d
Abhi Singh <thakurabhi5027@gmail.com>
2022-02-18 23:13:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-console.log("hello Everyone")
+console.log("hello babies")
 // 'https://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=e8a9af54d7234a9eb52ae8a33681770a'
 
 
 // e8a9af54d7234a9eb52ae8a33681770a
 const xhr = new XMLHttpRequest();
-xhr.open('GET','https://gnews.io/api/v4/top-headlines?&country=in&lang=en&token=a1d13eb31b7504d895e717156227568e', true);
+xhr.open('GET', 'https://gnews.io/api/v4/top-headlines?&country=in&lang=en&token=a1d13eb31b7504d895e717156227568e', true);
 xhr.onload = function () {
     let na = document.getElementById('newsAccordion')
     if (this.status === 200) {
